5cc3f64eb82e3ed74bb8c15b510a3695518df31b
jaketaylorpro <jaketaylorpro@gmail.com>
2017-03-10 00:32:18 -0500

    removed cpu_only option

--- examples/cpp_classification/classification.cpp
+++ examples/cpp_classification/classification.cpp
@@ -50,11 +50,7 @@ Classifier::Classifier(const string& model_file,
                        const string& trained_file,
                        const string& mean_file,
                        const string& label_file) {
-#ifdef CPU_ONLY
-  Caffe::set_mode(Caffe::CPU);
-#else
   Caffe::set_mode(Caffe::GPU);
-#endif
 
   /* Load the network. */
   net_.reset(new Net<float>(model_file, TEST));
